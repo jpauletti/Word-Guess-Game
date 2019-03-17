fb7537845e8ec0476231cc6c425c894ed3f73b1d
Julia Pauletti <julia.pauletti@yahoo.com>
2019-03-17 15:13:54 -0500

    start game function fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,6 +22,7 @@ var words = [
     }
 ];
 
+// only acceptable guesses
 var validGuesses = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 // array to push in letters that have been guessed
@@ -29,35 +30,54 @@ var lettersGuessed = [];
 var wins = 0;
 var remainingGuesses = 12;
 
-// <p> for the word to display
+// DOM variables
 var chosenWordText = document.getElementById('chosen-word');
 var lettersGuessedText = document.getElementById('letters-guessed');
 var remainingGuessesText = document.getElementById('remaining-guesses');
 var winsText = document.getElementById('wins');
 var playAgainButton = document.getElementById('play-again');
 
+// start game - choose random word, don't show letters
 function startGame() {
-    var chosenWord = words[Math.floor(Math.random() * words.length)].song.toLowerCase();
+    chosenWord = words[Math.floor(Math.random() * words.length)].song.toLowerCase();
+    playAgainButton.classList.remove('show');
     console.log('game started');
 
-        // start with blank <li>s
-    // for (var i = 0; i < chosenWord.length; i++) {
-    //     chosenWordText.innerHTML = chosenWordText.innerHTML + '<li id="' + i + '"></li>'
-    // }    
+    // reset guesses
+    remainingGuesses = 12;
+    // show new guesses left on page
+    remainingGuessesText.textContent = remainingGuesses;
+    // reset letters guessed
+    lettersGuessed = [];
+    lettersGuessedText.innerHTML = '';
+
+    // make text black again
+    chosenWordText.style.color = 'black';
+
+    //clear out current letters
+    chosenWordText.innerHTML = '';
+
+    // start with blank <li>s
+    for (var i = 0; i < chosenWord.length; i++) {
+        chosenWordText.innerHTML = chosenWordText.innerHTML + '<li id="' + i + '"></li>'
+    }   
 }
 
 // get random song name from words array
 var chosenWord = words[Math.floor(Math.random()*words.length)].song.toLowerCase();
 
+// reset remainingGuesses and  lettersGuessed
 function reset() {
     // reset guesses
     remainingGuesses = 12;
     // show new guesses left on page
     remainingGuessesText.textContent = remainingGuesses;
     // reset letters guessed
+    lettersGuessed = [];
     lettersGuessedText.innerHTML = '';
 }
 
+// show letter if it's in the word
 function revealLetter() {
     // when a correct letter is guessed, returns true
     if (chosenWord.includes(letter)) {
@@ -70,6 +90,7 @@ function revealLetter() {
     }
 }
 
+// subtract 1 from guesses and update page
 function updateGuesses() {
     // subtract one guess
     remainingGuesses = remainingGuesses - 1;
@@ -78,36 +99,34 @@ function updateGuesses() {
 
 }
 
+// if fail - reveal all letters
 function endGame() {
+    console.log('end game you lost');
     for (i = 0; i < chosenWord.length; i++) {
         // reveal all letters
         document.getElementById(i.toString()).textContent = chosenWord[i];
         chosenWordText.style.color = 'red';
     }
+    playAgainButton.classList.add('show');
+    // playAgainButton.addEventListener('click', startGame());
 }
 
+// if win
 function youWin() {
+    console.log('you win');
     wins = wins + 1;
     winsText.textContent = wins;
     chosenWordText.style.color = 'green';
     playAgainButton.classList.add('show');
+    // playAgainButton.addEventListener('click', startGame);
 }
 
-// set text content of the word on the page
-// chosenWordText.textContent = chosenWord;
-// for (var i = 0; i < chosenWord.length; i++) {
-//     chosenWordText.innerHTML = chosenWordText.innerHTML + '<span>' + chosenWord[i] + '</span>'
-// }
+
+startGame();
 
 // for (var i = 0; i < chosenWord.length; i++) {
-//     chosenWordText.innerHTML = chosenWordText.innerHTML + '<li>' + chosenWord[i] + '</li>'
+//     chosenWordText.innerHTML = chosenWordText.innerHTML + '<li id="' + i + '"></li>'
 // }
-
-// startGame();
-
-for (var i = 0; i < chosenWord.length; i++) {
-    chosenWordText.innerHTML = chosenWordText.innerHTML + '<li id="' + i + '"></li>'
-}
 
 
 
@@ -115,7 +134,9 @@ for (var i = 0; i < chosenWord.length; i++) {
 if (remainingGuesses > -1) {
     document.onkeyup = function (event) {
         var letter = event.key.toLowerCase();
+        console.log(letter);
 
+        // if it's a letter
         if (validGuesses.indexOf(letter) > -1) {
             // if letter hasn't been guessed already
             if (lettersGuessed.indexOf(letter) === -1) {
@@ -124,7 +145,7 @@ if (remainingGuesses > -1) {
                     // add letter to array of guessed letters
                     lettersGuessed.push(letter);
                     // show new guessed letter on page
-                    lettersGuessedText.innerHTML = lettersGuessedText.innerHTML + '<span>' + lettersGuessed[lettersGuessed.length - 1] + '</span>';
+                    lettersGuessedText.innerHTML = lettersGuessedText.innerHTML + '<span>' + lettersGuessed[lettersGuessed.length - 1] + '</span>'; // add guessed letter to page
                 }
 
 
@@ -134,7 +155,7 @@ if (remainingGuesses > -1) {
                     for (i = 0; i < chosenWord.length; i++) {
                         if (chosenWord[i] === letter)
                             // reveal letter in word on page
-                            document.getElementById(i.toString()).textContent = chosenWord[i];
+                            document.getElementById(i.toString()).textContent = chosenWord[i]; // reveal letter in word
                     }
 
 
@@ -153,20 +174,20 @@ if (remainingGuesses > -1) {
                 }
 
                 if (emptyLis === 0 && remainingGuesses > 0) {
-                    reset();
                     youWin();
                 }
 
-                playAgainButton.addEventListener('click', startGame());
+                // playAgainButton.addEventListener('click', startGame());
 
-            }
-            // don't let user keep guessing when remainingGuesses = 0
-            if (remainingGuesses > 0) {
-                updateGuesses();
-            }
+                // don't let user keep guessing when remainingGuesses = 0
+                if (remainingGuesses > 0) {
+                    updateGuesses();
+                }
 
-            if (remainingGuesses === 0) {
-                endGame();
+                if (remainingGuesses === 0) {
+                    endGame();
+                }
+
             }
 
         }
